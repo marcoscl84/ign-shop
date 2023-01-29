4229f8020b366fb766862411e3172528c6bb11f4
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-01-29 17:54:24 -0300

    feat: estilos globais

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+import { globarStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+
+globarStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
