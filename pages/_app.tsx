21fbf88830049c37242db141028606a66a50ecef
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-02-04 13:51:14 -0300

    feat: cabeçalho da aplicação

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { globarStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
 import type { AppProps } from "next/app";
+import Image from "next/image";
 
-globarStyles();
+import logoImg from "../assets/logo.svg";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globarStyles();
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
