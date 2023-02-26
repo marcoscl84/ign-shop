1575037f0a5571e16165a331d4f88fa24f6d6766
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-02-25 22:37:41 -0300

    feat: estrutura pág sucesso

--- pages/success/index.tsx
+++ pages/success/index.tsx
@@ -1,3 +1,19 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efeutada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Daleee <strong>meu rei</strong>, tua camiseta{" "}
+        <strong>sei lá o nome</strong>, já tá a caminho da tua casa.
+      </p>
+
+      <Link href={""}>Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
