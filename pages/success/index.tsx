0d035090eb13145598fc42e9798a21986a55ac90
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-02-26 12:47:29 -0300

    feat: apresentação dados da compra

--- pages/success/index.tsx
+++ pages/success/index.tsx
@@ -1,19 +1,65 @@
 import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import { GetServerSideProps } from "next";
+import Image from "next/image";
 import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efeutada!</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} alt={""} width={120} height={110} />
+      </ImageContainer>
 
       <p>
-        Daleee <strong>meu rei</strong>, tua camiseta{" "}
-        <strong>sei lá o nome</strong>, já tá a caminho da tua casa.
+        Daleee <strong>{customerName}</strong>, tua camiseta{" "}
+        <strong>{product.name}</strong>, já tá a caminho da tua casa.
       </p>
 
       <Link href={""}>Voltar ao catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  params,
+}) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  console.log(session);
+  console.log(session.line_items?.data);
+
+  const customerName = session.customer_details?.name;
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
+
+// Fetch pode ser feito de trÊs formas:
+//  - Client side (useEffect) -> não utilizado por algumas restrições na chamada da api.
+//  - getServerSideProps -> opção ideal
+//  - getStaticProps -> não faz sentido nesse caso porque viria sempre os mesmos dados na exibição da página
